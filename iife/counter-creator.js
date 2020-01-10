6e4a32d0fc8de9be6a93c3069df3426e96b5c484
rohinivsenthil <rohinivsenthil@gmail.com>
2020-01-10 10:10:29 +0530

    Implemented counter creator

--- iife/counter-creator.js
+++ iife/counter-creator.js
@@ -1,21 +1,11 @@
-
-var counter = function () {
-    var count = 0;
-    return function () {
-        ++count;
-        return count;
-        }
+var counter_array = (function countCreator(num_of_counters = 5){
+    let counter_arr =[]
+    for(var i = 0; i < num_of_counters; i++ ){
+        counter_arr.push(function counter(){
+            let count = 0;
+            return ++ count;
+        })
     }
-
-function counterCreator(num_of_counters){
-    var counterArray = [];
-    for(var i = 0; i < num_of_counters; i++){
-        counterArray[i] = counter();
-    }
-    return counterArray;
-}
-
-var counters = counterCreator(2);
-console.log(counters[0]());
-console.log(counters[0]());
-console.log(counters[1]());
+    return counter_arr;
+})()
+console.log(counter_array);
